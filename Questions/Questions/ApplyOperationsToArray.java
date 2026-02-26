4af42c03a252d35ab5a9b28cd193ed1583468a36
Anurag Udapure <anuragudapure@gmail.com>
2026-02-26 16:30:31 +0530

    Added Sum Of Roots To Leaf Binary Numbers Question

--- Questions/Questions/ApplyOperationsToArray.java
+++ Questions/Questions/ApplyOperationsToArray.java
@@ -8,7 +8,7 @@ class Solution {
                 nums[i+1] = 0;
             }
         }
-        //Moving all zeroes to right
+         //Moving all zeroes to right
          int[] arr = new int[n];
          int j = 0;
          for(int i = 0; i < n; i++){
---
+++ Questions/Questions/Solution.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class solution{
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
+        if(arr[0] == ')' || arr[0] == ']' || arr[0] == '}'){
+            return false;
+        }
+        for(char i : arr){
+            if((i == '(' || i == '[' || i == '{')){
+                stack.push(i);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if(i == ')' && top!= '('){
+                    return false;
+                }
+                if(i == ']' && top!= '['){
+                    return false;
+                }
+                if(i == '}' && top!= '{'){
+                    return false;
+                }
+            } 
+        }
+        return stack.isEmpty();
+    }
+}
