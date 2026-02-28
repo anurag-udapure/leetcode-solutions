127ae0c96ca928559ac801953df1192c6d49cedd
Anurag Udapure <anuragudapure@gmail.com>
2026-02-28 17:06:45 +0530

    Added Leetcode Question No. 1680

---
+++ Questions/ConcatenationOfConsecutiveBinaryNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long result = 0;
+        int MOD = 1000000007;
+        int bitLength = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                bitLength++;
+            }
+            result = ((result << bitLength) + i) % MOD;
+        }
+        
+        return (int) result;
+    }
+}
