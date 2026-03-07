5cb08209ceaba41934a3524da3ac4ad4c3a0add3
Anurag Udapure <anuragudapure@gmail.com>
2026-03-07 21:45:48 +0530

    Added Solution for Transformed Array.

---
+++ Questions/TransformedArray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                result[i] = nums[i];
+            } else {
+                int idx = ((i + nums[i]) % n + n) % n;
+                result[i] = nums[idx];
+            }
+        }
+        return result;
+    }
+}
