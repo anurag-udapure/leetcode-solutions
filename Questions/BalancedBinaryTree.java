4671b2c94c37bc85969f25453d1ed909e5c6f232
Anurag Udapure <anuragudapure@gmail.com>
2026-03-09 21:12:41 +0530

    Added Solution for Balanced Binary Tree.

---
+++ Questions/BalancedBinaryTree.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        int left = getMaxSubTreeHeight(root.left);
+        int right = getMaxSubTreeHeight(root.right);
+        return (Math.abs(left-right) <= 1 && isBalanced(root.left) && isBalanced(root.right));
+    }
+    private static int getMaxSubTreeHeight(TreeNode node){
+        if(node ==  null) return 0;
+        return 1 + Math.max((getMaxSubTreeHeight(node.left)),(getMaxSubTreeHeight(node.right)));
+    }
+}
